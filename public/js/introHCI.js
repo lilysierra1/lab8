7c84caa9fe2bb56760a103de7df3536a57653c0d
vagrant <vagrant@precise32.(none)>
2014-02-26 23:00:30 +0000

    analytics lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -3,11 +3,17 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+
 });
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".likeBtn").click(likeListener);
 }
+
+function likeListener(e) {
+	ga('send', 'event', 'like', 'liked');
+}
+
